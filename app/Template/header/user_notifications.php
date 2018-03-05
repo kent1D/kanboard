adfa741543e1fc49531591a3afd31823a6504651
kent1 <kent1@arscenic.info>
2018-03-05 13:09:46 +0100

    Add a badge telling the number of unread notifications

--- app/Template/header/user_notifications.php
+++ app/Template/header/user_notifications.php
@@ -1,6 +1,6 @@
 <span class="notification">
 <?php if ($this->user->hasNotifications()): ?>
-    <?= $this->modal->mediumIcon('bell web-notification-icon', t('Unread notifications'), 'WebNotificationController', 'show', array('user_id' => $this->user->getId())) ?>
+    <?= $this->modal->medium('bell web-notification-icon', '<i class="badge js-modal-medium">'.$this->user->countNotifications().'</i>', 'WebNotificationController', 'show', array('user_id' => $this->user->getId()), t('Unread notifications')) ?>
 <?php else: ?>
     <?= $this->modal->mediumIcon('bell', t('My notifications'), 'WebNotificationController', 'show', array('user_id' => $this->user->getId())) ?>
 <?php endif ?>
